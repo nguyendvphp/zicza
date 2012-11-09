1b794b4fce00012c0f294578dcc3cdd07c8ed9d1
nguyendvphp <nguyendvphp@gmail.com>
2012-11-09 14:13:39 +0700

    change url file adm/js/global.js

--- adm/js/global.js
+++ adm/js/global.js
@@ -1,5 +1,5 @@
 
-ADM_HOST_PATH = 'http://localhost/gallery/src/adm/';
+ADM_HOST_PATH = 'http://localhost/zicza/zicza/adm/';
 
 COMFIRM = "Xác nhận thay đổi trạng thái";
 //ACTIVE = 'Bạn chắc chắn muốn kích hoạt người dùng.';
@@ -7,28 +7,15 @@ COMFIRM = "Xác nhận thay đổi trạng thái";
 ACTIVE ='ACTIVE';
 INACTIVE ='INACTIVE';
 EDITTING = 'EDITTING';
-ACTIVE_GROUP = 'Bạn chắc chắn muốn kích hoạt nhóm.';
-INACTIVE_GROUP = 'Bạn chắc chắn muốn bỏ kích hoạt nhóm.';
-
-ACTIVE_CATEGORY = 'Bạn chắc chắn muốn kích hoạt danh  mục.';
-INACTIVE_CATEGORY = 'Bạn chắc chắn muốn bỏ kích hoạt danh mục.';
-
-ACTIVE_ADV = 'Bạn chắc chắn muốn kích hoạt quảng cáo.';
-INACTIVE_ADV = 'Bạn chắc chắn muốn bỏ kích quảng cáo.';
-
-ACTIVE_GAME =   'Bạn chắc chắn muốn chuyển game này sang trạng thái kích hoạt';
-INACTIVE_GAME = 'Bạn chắc chắn muốn chuyển game này sang trạng thái chờ duyệt';
-EDITTING_GAME = 'Bạn chắc chắn muốn chuyển game này sang trạng thái đang sửa';
 
 
 WARNING = 'Thông báo';
 WARNING_PHONE = 'Bạn phải nhập số điện thoại vào để tìm kiếm';
 
 //img = 'http://localhost/mgame_beta/src/uploads/lightbox-ico-loading.gif';
-img = 'http://localhost/gallery/src/uploads/lightbox-ico-loading.gif';
+img = 'http://localhost/zicza/zicza/uploads/lightbox-ico-loading.gif';
 
-CHOOSE_FILE = 'Bạn chưa chọn file games';
-NOT_HANDSET = 'Không có điện thoại nào được chọn';
+
 
 WARNING = 'Thông báo';
 
